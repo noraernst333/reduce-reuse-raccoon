8e91d868097ab28c75b41e3bcefe85b6978f6359
Nora <noraernst333@gmail.com>
2026-05-03 18:42:18 -0400

    copied JS file

---
+++ js/script.js
@@ -0,0 +1,13 @@
+  const btn = document.getElementById("topBtn");
+
+  window.onscroll = function () {
+    if (window.scrollY > 200) {
+      btn.style.display = "block";
+    } else {
+      btn.style.display = "none";
+    }
+  };
+
+  btn.onclick = function () {
+    window.scrollTo(0, 0);
+  };
